ff02d4320e6e24bd6d89592eff5cba6bfd1b730f
tysion <tysion@gmail.com>
2025-06-03 21:58:40 +0545

    first commit

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+import LaptopCarousel from "../Component/LaptopCarousel";
+
+import News from "../Components/News";
+import Footer from "../Components/Footer";
+
+const Home = () => {
+  return (
+    <>
+      <News />
+      <LaptopCarousel />
+
+      <Footer />
+    </>
+  );
+};
+
+export default Home;
