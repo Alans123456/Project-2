c258e64a8db0f38ed48a354267431f06d2f46c4a
tysion <ctit23.ala@ismt.edu.np>
2025-06-05 15:56:26 +0545

    What We offer

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -7,6 +7,7 @@ import Footer from "../Components/Footer";
 import Hero from "../Components/About/Hero";
 import AboutDetail from "../Components/About/AboutDetail";
 import OurMission from "../Components/About/OurMission";
+import WhatWeOffer from "../Components/About/WhatWeOffer";
 
 const Home = () => {
   return (
@@ -14,6 +15,7 @@ const Home = () => {
       <Hero />
       <AboutDetail />
       <OurMission />
+      <WhatWeOffer />
       <News />
       <LaptopCarousel />
 
